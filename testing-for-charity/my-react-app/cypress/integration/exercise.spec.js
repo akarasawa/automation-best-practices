539a9e0137e4f694e679890d46cd973bd397a733
Gvero, Ashleigh <agvero@conversantmedia.com>
2021-10-14 11:41:41 -0500

    add yml

--- testing-for-charity/my-react-app/cypress/integration/exercise.spec.js
+++ testing-for-charity/my-react-app/cypress/integration/exercise.spec.js
@@ -1,29 +1,35 @@
 /// <reference types="cypress" />
 
-describe('Exercise', () => {
-    it('loads', ()=> {
+describe("Exercise", () => {
+    it("loads", () => {
         /** Your code below */
-    
+
         //1. Use cy.visit('/') to go to the app url
+        cy.visit("/");
         //2. Use cy.get('.App-link').should('be.visible') to assert valid state
+        cy.get(".App-link").should("be.visible");
         /** Your code above */
-    })
-    
-    it('link goes to ultimateqa', ()=> {
+    });
+
+    it("link goes to ultimateqa", () => {
         /** Your code below */
-    
         // 1. Use cy.visit('/') to go to the app url
+        cy.visit("/");
         // 2. cy.get('').should('have.attr', 'href').and('include', 'ultimateqa.com')
-    
+        cy.get(".App-link")
+            .should("have.attr", "href")
+            .and("include", "ultimateqa.com");
         /** Your code above */
-    })
-    
-    it('should open link in new tab',()=> {
+    });
+
+    it("should open link in new tab", () => {
         /** Your code below */
-    
         // 1. Cmon, you know how to open the url now :)
+        cy.visit("/");
         // 2. cy.get('').should('have.attr', 'WHAT').and('include', 'WHAT VALUE')
-    
+        cy.get(".App-link")
+            .should("have.attr", "target")
+            .and("include", "_blank");
         /** Your code above */
-    })  
-})
+    });
+});
